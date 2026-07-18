7af029045145a94423677b48fa36b4aa8429d64c
Kelsey <gabriellemkelsey@gmail.com>
2026-07-18 23:44:50 +0800

    Update Task model

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -7,20 +7,37 @@ from pydantic import BaseModel
 class Task(BaseModel):
     """
     Represents a task inside Elliotte.
-    
-    """
 
+    """
     id: str
+
     title: str
 
-    status: str = "Todo"
-
-    priority: str = "Medium"
-
-    estimated_hours: Optional[float] = None
+    status: str
 
     due_date: Optional[date] = None
 
-    category: Optional[str] = None
+    course: Optional[str] = None
 
-    can_split: bool = True
+    task_type: Optional[str] = None
+
+    size: Optional[str] = None
+
+    completed: bool = False
+
+
+    # Previous Model (save for future reference)
+    # id: str
+    # title: str
+
+    # status: str = "Todo"
+
+    # priority: str = "Medium"
+
+    # estimated_hours: Optional[float] = None
+
+    # due_date: Optional[date] = None
+
+    # category: Optional[str] = None
+
+    # can_split: bool = True
